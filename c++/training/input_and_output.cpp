ba8157d13c20070a213338d59692f76970985cd2
kan <56210792+chakbot@users.noreply.github.com>
2019-10-09 20:45:14 -0400

    learning cpp

---
+++ c++/training/input_and_output.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int sum = 0;
+    int i = 0;
+    while (cin >> i) {
+        sum += i;
+    }
+    cout << sum << endl;
+}
